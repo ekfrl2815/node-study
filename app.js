e01fa5d8183ea7e7c5c55cbb5c59403dd81aa2b3
dog <08314@skcc.com>
2016-10-13 16:43:01 +0900

    add package.json

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('yahoo!!!!!!');
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+
+console.log(`-> ${request.method} ${request.url}`);
+response.end('welcome');
+
+}).listen(8080); // 이 서버를 활성화하고 8080 포트로 받습니다.
